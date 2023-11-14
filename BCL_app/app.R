549c4793788b3e6e835497439de93593adbe9cbf
Daniel Robinson <113081861+danierob@users.noreply.github.com>
2023-11-14 12:04:31 -0800

    Working on BC liquor dataset it looks like. Added a beer image, treid a bunch of other stuff that didn't work so now I'm just going to add a widget and move on.

---
+++ BCL_app/app.R
@@ -0,0 +1,71 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+
+
+# load packages
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+#load in data
+bcl <- read.csv("data/bcl-data.csv", stringsAsFactors = FALSE)
+
+#ui
+ui <- fluidPage(
+  titlePanel("BC Liquor Store prices"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("priceInput", "Price", 0, 100, c(25, 40), pre = "$"),
+      radioButtons("typeInput", "Product type",
+                   choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                   selected = "WINE"),
+      uiOutput("countryOutput"),
+      img(src = "beer.png", height = 150, width = 150) #added beer image
+    ),
+    mainPanel(
+      plotOutput("coolplot"),
+      br(), br(),
+      tableOutput("results")
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$countryOutput <- renderUI({
+    selectInput("countryInput", "Country",
+                sort(unique(bcl$Country)),
+                selected = "CANADA")
+  })
+
+  filtered <- reactive({
+    if (is.null(input$countryInput)) {
+      return(NULL)
+    }
+
+    bcl %>%
+      filter(Price >= input$priceInput[1],
+             Price <= input$priceInput[2],
+             Type == input$typeInput,
+             Country == input$countryInput
+      )
+  })
+
+  output$coolplot <- renderPlot({
+    if (is.null(filtered())) {
+      return()
+    }
+    ggplot(filtered(), aes(Alcohol_Content)) +
+      geom_histogram()
+  })
+
+  output$results <- renderTable({
+    filtered()
+  })
+}
+
+shinyApp(ui = ui, server = server)
