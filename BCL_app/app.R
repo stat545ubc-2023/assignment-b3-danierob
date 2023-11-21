845805dfbeaef1e145b8fc3225c3d3c80d22ffda
Daniel Robinson <113081861+danierob@users.noreply.github.com>
2023-11-20 18:27:20 -0800

    added some more annotation to code lines

--- BCL_app/app.R
+++ BCL_app/app.R
@@ -17,42 +17,45 @@ bcl <- read.csv("data/bcl-data.csv", stringsAsFactors = FALSE)
 
 #ui
 ui <- fluidPage(
-  titlePanel("BC Liquor Store prices"),
-  sidebarLayout(
+  titlePanel("BC Liquor Store prices"),             #add title
+  sidebarLayout(                                    #create sidebar
     sidebarPanel(
-      sliderInput("priceInput", "Price", 0, 100, c(0, 100), pre = "$"),
-      radioButtons("typeInput", "Product type",
-                   choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+      sliderInput("priceInput", "Price",            #slider widget ui input
+                    0, 100, c(0, 100), pre = "$"),
+      radioButtons("typeInput", "Product type",     #radio button widget ui input
+                   choices = c("BEER",
+                   "REFRESHMENT", "SPIRITS",
+                   "WINE"),
                    selected = "WINE"),
-      uiOutput("countryOutput"),
-      uiOutput("subtypeOutput"),                     #added to create a subtype dropdown widget
+      uiOutput("countryOutput"),                     #dropdown widget ui input
+      uiOutput("subtypeOutput"),                     #!added to create a subtype dropdown widget
       img(src = "beer.png", height = 200,
-          width = 200, align = "center")             #added beer image
+          width = 200, align = "center")             #!added beer image
     ),
-    mainPanel(
-      plotOutput("coolplot"),
+    mainPanel(                                       #create main panel
+      plotOutput("coolplot"),                        #placeholder in ui for "coolplot" that will be defined in server
       br(), br(),
-      tableOutput("results")
+      tableOutput("results")                         #table element in ui that will hold table element as defined in server
     )
   )
 )
 
-server <- function(input, output) {
-  output$countryOutput <- renderUI({
-    selectInput("countryInput", "Country",
-                c("ALL", sort(unique(bcl$Country))), # change code to add an ALL column
-                selected = "ALL")                    # change ALL to default
+server <- function(input, output) {                  #server is responsible for listening to changes to the inputs and creating outputs to show in the app
+  output$countryOutput <- renderUI({                 #save object to "output" build object with "render"
+    selectInput("countryInput", "Country",           #access "input" values
+                c("ALL", sort(unique(bcl$Country))), #!change code to add an ALL column
+                selected = "ALL")                    #!change ALL to default
   })
 
 
-# Render subtype dropdown based on selected type
+#!Render subtype dropdown based on selected type
   output$subtypeOutput <- renderUI({
     if (is.null(input$typeInput)) {
       return(NULL)
     }
 
 
-# Get subtypes from the bcl table and define "ALL" for widget option
+#!Get subtypes from the bcl table and define "ALL" for widget option
     subtypes_for_type <- c("ALL", unique(bcl$Subtype[bcl$Type == input$typeInput]))
 
     selectInput("subtypeInput", "Subtype",
@@ -60,17 +63,17 @@ server <- function(input, output) {
                 selected = "ALL")                   #select ALL to default
   })
 
-
-  filtered <- reactive({
+#defining reactive variable "filtered" to hold the reactive dataset
+  filtered <- reactive({                            #"reactive" to rerun all reactive variables with each update
     if (is.null(input$countryInput)) {
       return(NULL)
     }
 
-    bcl %>%
+    bcl %>%                                         #dataset of all reactive variables
       filter(Price >= input$priceInput[1],
              Price <= input$priceInput[2],
              Type == input$typeInput,
-                                                    # Change countryInput and subtypeInput to reflect "ALL"
+                                                    #!Change countryInput and subtypeInput to reflect "ALL"
              (input$countryInput == "ALL" | Country == input$countryInput),
              (input$subtypeInput == "ALL" | Subtype == input$subtypeInput)
       )
@@ -80,8 +83,9 @@ server <- function(input, output) {
     if (is.null(filtered())) {
       return()
     }
-    ggplot(filtered(), aes(Alcohol_Content)) +
-      geom_histogram(fill = "#FFCC00", color = "grey")
+    ggplot(filtered(), aes(Alcohol_Content)) +      #plot creation
+      geom_histogram(fill = "#FFCC00",
+                     color = "grey")
   })
 
 
